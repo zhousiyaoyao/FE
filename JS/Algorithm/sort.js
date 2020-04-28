37b711e290dc1943d982c47b1c5ddbfe1d3a658a
周思尧 <1094290934@qq.com>
2020-04-28 09:45:59 +0800

    999

--- JS/Algorithm/sort.js
+++ JS/Algorithm/sort.js
@@ -1,4 +1,53 @@
-//冒泡排序 稳定，n平方
+// 快排
+function quickSort(array, start, end) {
+
+    let length = array.length;
+  
+    // 如果不是数组或者数组长度小于等于1，直接返回，不需要排序 
+    if (!Array.isArray(array) || length <= 1 || start >= end) return;
+  
+    let index = partition(array, start, end); // 将数组划分为两部分，并返回右部分的第一个元素的索引值
+  
+    quickSort(array, start, index - 1); // 递归排序左半部分
+    quickSort(array, index + 1, end); // 递归排序右半部分
+}
+  
+const partition = (arr, start, end) =>{
+    var pivot = arr[start]
+    while(start < end){
+        while(arr[end] >= pivot && start < end){
+            end--
+        }
+        arr[start] = arr[end]
+        while(arr[start] < pivot && start < end){
+            start++
+        }
+        arr[end] = arr[start]
+    }
+    arr[start] = pivot
+    return start
+}
+
+// 三路快排
+const partition3way = (arr) =>{
+    var start = 0
+    var end = arr.length - 1
+    var  i = 0 
+    var pivot = arr[0]
+    var swap = (a, i, j) => [a[i],a[j]] = [a[j],a[i]]
+    while(i <= end){
+        if(arr[i] < pivot){
+            swap(arr, start++, i++)
+        }
+        else if(arr[i] > pivot){
+            swap(arr, i, end--)
+        }else{
+            i++
+        }
+    }
+}
+
+// 冒泡排序 稳定，n平方
 function bubblesort(a){
     if(!Array.isArray(a) || a.length <= 1){
         return
@@ -22,7 +71,7 @@ function bubblesort(a){
     return a
 }
 
-//选择排序 不稳定，n平方
+// 选择排序 不稳定，n平方
 function selectsort(a){
     let length = a.length
     if(!Array.isArray(a) || a.length <= 1){
@@ -46,8 +95,7 @@ function swap(array, left, right){
     array[right] = array[left]
 }
 
-//插入排序，稳定，n平方
-
+// 插入排序，稳定，n平方
 function insertsort(a){
     let length = a.length
     if(!Array.isArray(a) || a.length<=1){
@@ -65,9 +113,4 @@ function insertsort(a){
     return a
 }
 
-//希尔排序，nlogn，不稳定
-
-
-var arr = [4,3,7,9,5,7,1,3]
-var b = selectsort(arr)
-console.log(b)
+// 希尔排序，nlogn，不稳定
--- readme.md
+++ readme.md
@@ -1,13 +1,13 @@
 # 前端基础实践
-### [算法总结]
+### 算法总结
 * [Leetcode hot 100](https://github.com/zhousiyaoyao/FE/blob/master/JS/Algorithm/leetcode_hot_100.js)
 * [ScriotOj](https://github.com/zhousiyaoyao/FE/blob/master/JS/Algorithm/ScriptOj.js)
 * [Tree](https://github.com/zhousiyaoyao/FE/blob/master/JS/Algorithm/tree_iterate.js)
 ### [JS总结](https://github.com/zhousiyaoyao/FE/blob/master/JS/js.md)
-### [CSS总结]
-### [HTML总结]
 ### [Webpack总结](https://github.com/zhousiyaoyao/FE/blob/master/Project/webpack.md)
-### [框架总结]
-### [Typescript总结]
 ### [源码总结](https://github.com/zhousiyaoyao/FE/blob/master/Source_code/vue_source_code.md)
 ### [demo总结]
+### [框架总结]
+### [Typescript总结]
+### [CSS总结]
+### [HTML总结]
